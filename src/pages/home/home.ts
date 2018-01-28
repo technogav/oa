9cfda5d46adba7c19a6dd09526ff90d496a2464d
technogav <gavin.murphy@webelevate.ie>
2018-01-28 14:57:45 +0000

    first commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  getViewPort(){
+    let w = window,
+    d = document,
+    e = d.documentElement,
+    g = d.getElementsByTagName('body')[0],
+    x = w.innerWidth || e.clientWidth || g.clientWidth,
+    y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+    console.log(x + ' × ' + y); 
+    
+  }
+
+  ionViewDidEnter(){
+    this.getViewPort();
+  }
+  
+
+}
